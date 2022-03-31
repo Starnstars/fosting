271e3c426696992eeec24ec04662a1af39cef5ed
Maestro-lee <alstlr815@gmail.com>
2022-03-31 17:26:36 +0900

    CD deploy.sh, .travis.yml appspec.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl fosting | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f1 fosting | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -22,6 +22,7 @@ else
 fi
 
 echo ">새 애플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
